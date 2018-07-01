2fc45f95547c36511917b78902757c1694ea8c8d
Brian Wu <bwubrian@gmail.com>
2018-07-01 00:05:29 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+
+import discord
+from discord.ext import commands
+import asyncio
+import time
+import math
+
+bot = commands.Bot(command_prefix = ["n!", "n1", "b2", "n2", "N!", "N1", "B2", "N2"] , description = "A super trivia bot that tries not to suck too much.")
+@bot.command()
+async def say(ctx, *, something = None):
+	"""Prints the given string."""
+	if something is not None:
+		await ctx.send(something)
+	else:
+		await ctx.send("lol you're pretty bad at this, use the *say* command with argument, such as: **n!say ooglyoogly**")
+    
+
+@bot.event
+async def on_ready():
+	print("I am running on " + bot.user.name)
+	print("With the ID: " + str(bot.user.id))
+
+	await bot.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = "David stuck in Plat. [n!]"))
+
+
+
+bot.run("NDU3NjMwNzM0NjE4ODUzMzc4.Dgb5rQ.9o1Ir3Rch0I46nWclwaM1b6xfrc")
